a910cca4e8aeb6a4f11c98bb95033aaf46cd9db8
Joseph Ma <josephma293@gmail.com>
2024-08-17 11:07:20 -0700

    Feat: Add boilerplate

--- backend/script.py
+++ backend/script.py
@@ -1,22 +1,30 @@
 import json
 from typing import List
 
-from anime_types import InputAnimeData, AnimeData
+from anime_types import AnimeEntry, InputAnimeData
 
-def process(data: List[InputAnimeData]) -> AnimeData:
+def _process_entry(data: InputAnimeData) -> AnimeEntry:
     print(data)
 
     return {
-        "username": "joe",
-        "data": [],
-        "timestamp": ""
+        "name": "abc",
+        "based": 75,
+        "age": 15,
+        "degen": 75,
+        "normie": 90,
+        "completion": 50,
+        "ranked": 75,
     }
+
+def process(data: List[InputAnimeData]) -> List[AnimeEntry]:
+    """Process raw data from MAL into a list of entries for ZK Circuit"""
+    return [_process_entry(entry) for entry in data]
 
 file_path = './dump.json'
 try:
     with open(file_path, 'r') as file:
         data = json.load(file)
-    print(data['data'])
+    process(data['data'])
 except FileNotFoundError:
     print(f"Error: File '{file_path}' not found.")
 except json.JSONDecodeError:
